bd14971221d69d4346ab2a8000b2ba1d79bad2e9
Taylir <NatureProd@proton.me>
2026-01-08 10:07:18 -0500

    Make a compare cards function for the game

--- scripts.ts
+++ scripts.ts
@@ -20,16 +20,28 @@ function shuffleNewDeck(deck: string[]): string[] {
 
   for (let i = 0; i < 52; i++) {
     const selectedIndex: number = Math.floor(Math.random() * deckCopy.length);
-    console.log(selectedIndex)
     shuffledDeck.push(deckCopy[selectedIndex]!);
     deckCopy.splice(selectedIndex, 1);
   }
   return shuffledDeck;
 }
-
 const shuffledDeck = shuffleNewDeck(makeNewDeck());
 
-console.log(shuffledDeck, shuffledDeck.length);
+function compareCards(card1: string, card2: string): number {
+  const card1Value: number = baseDeck.deckValues.indexOf(card1[0]!);
+  const card2Vaule: number = baseDeck.deckValues.indexOf(card2[0]!);
+  console.log(card1Value, card2Vaule)
+  if (card1Value > card2Vaule) {
+    return 1;
+  } else if (card1Value < card2Vaule) {
+    return -1;
+  } else {
+    return 0;
+  }
+}
+
+console.log(compareCards("3H", "KH"));
+
 
 
 
